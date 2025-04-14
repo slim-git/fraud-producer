e9ea44488bd1b584d4da73a3f43ca8336f0791d6
sghorbal <s.ghorbal@groupe-reussite.fr>
2025-04-14 17:01:41 +0200

    load .csv file once and keep it in cache memory

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import os
 import uvicorn
+import pandas as pd
+from datetime import datetime
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -6,10 +9,7 @@ from fastapi.templating import Jinja2Templates
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
-import pandas as pd
-from datetime import datetime
-import random
-import os
+from functools import lru_cache
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -48,20 +48,28 @@ templates = Jinja2Templates(directory="templates")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@lru_cache(maxsize=1)
+def get_full_dataframe():
+    """
+    Get the full dataframe from the CSV file.
+    This function is cached for performance reasons.
+    """
+    filename = "https://lead-program-assets.s3.eu-west-3.amazonaws.com/M05-Projects/fraudTest.csv"
+    return pd.read_csv(filename, header=0, index_col=[0])
+
 @app.get("/current-transactions")
 @limiter.limit(RATE_LIMIT)
 async def current_transactions(request: Request):
     """
     Return one current transaction in [`.to_json(orient="split")`](https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_json.html) format in Pandas.
     """
-    p = 0.001
-    filename = "https://lead-program-assets.s3.eu-west-3.amazonaws.com/M05-Projects/fraudTest.csv"
-    df = pd.read_csv(filename, header=0, index_col=[0] ,skiprows=lambda i: i>0 and random.random() > p)
+    df = get_full_dataframe()
     df = df.sample().loc[:, ~df.columns.isin(["trans_date_trans_time", "fraud", "unix_time"])]
     df["current_time"]= datetime.now()
     
     return df.to_json(orient="split")
 
+# ========================================================================
 if __name__=="__main__":
     PORT = os.getenv("PORT", 4000)
     uvicorn.run(app, host="0.0.0.0", port=PORT) 
